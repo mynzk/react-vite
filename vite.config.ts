0eb7cdd234cda2e6366aeef58eb4bdef06fffa67
zhang kui <1627528424@qq.com>
2023-02-14 17:27:55 +0800

    feat: 添加配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
+import eslintPlugin from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -11,7 +12,7 @@ export default defineConfig(({ mode }) => {
       open: true,
       port: 5000,
     },
-    plugins: [react()],
+    plugins: [react(), eslintPlugin()],
     resolve: {
       alias: {
         "@": resolve(__dirname, "./src"),
