05ee4a2ff7abaaf1a932935dde442011f4af8375
zhang kui <1627528424@qq.com>
2023-02-14 18:01:30 +0800

    ci: 配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,4 @@
+/** @type {import('cz-git').UserConfig} */
 module.exports = {
   ignores: [(commit) => commit.includes("init")],
   extends: ["@commitlint/config-conventional"],
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "@/assets/react.svg";
 import "./App.css";
-console.log(import.meta.env);
 
 function App() {
   const [count, setCount] = useState(0);
